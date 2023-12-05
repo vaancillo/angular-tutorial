f2e9d7e8a7a8891059c8f58acac48ee2d98a1514
vancillo <yaredbane@gmail.com>
2023-12-04 21:33:20 -0600

    Add UserComponent to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [CommonModule, RouterOutlet, UserComponent],
+  template: `<h1>Hello world from AngularJS </h1>
+  <app-user></app-user>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
